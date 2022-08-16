af765c007da390ee6969c99b6ad6b46d895ce152
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-17 01:01:14 +0900

    ADD - custom fetch and endpoints

---
+++ src/api/custom-fetch.ts
@@ -0,0 +1,56 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-param-reassign */
+import axios, { AxiosError } from 'axios';
+import { authTokenKey, persistStore } from '../persistStore/persistStore';
+import { backendBaseUrl } from './endpoints';
+
+export type FetchResponse<T> = { data: T };
+
+export interface ErrorResponse {
+  details: string;
+}
+
+const tokenMissingError = 'Token is missing';
+
+const apiBackend = axios.create({
+  baseURL: backendBaseUrl,
+  headers: {
+    Authorization: persistStore.get(authTokenKey) || '',
+  },
+});
+
+apiBackend.interceptors.request.use(
+  (config) => {
+    if (config.headers) config.headers.Authorization = persistStore.get(authTokenKey) || '';
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+apiBackend.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (error: AxiosError<ErrorResponse>): Promise<never> | undefined => {
+    const errorDetail = error.response?.data.details;
+    if (errorDetail === tokenMissingError) {
+      window.alert('다시 로그인을 진행해 주세요.');
+      window.history.pushState(null, '', '/');
+      window.location.reload();
+      return;
+    }
+    return Promise.reject(error);
+  },
+);
+
+export const apiFetch = {
+  get: <T>(url: string): Promise<T> => apiBackend.get(url).then((res) => res.data),
+
+  post: <T>(url: string, body: object): Promise<T> => apiBackend.post(url, body).then((res) => res.data),
+
+  delete: <T>(url: string): Promise<T> => apiBackend.delete(url).then((res) => res.data),
+
+  put: <T>(url: string, body: object): Promise<T> => apiBackend.put(url, body).then((res) => res.data),
+};
---
+++ src/api/endpoints.ts
@@ -0,0 +1,11 @@
+export const backendBaseUrl = 'https://5co7shqbsf.execute-api.ap-northeast-2.amazonaws.com/production/';
+
+export const apiEndpont = {
+  signUp: `/auth/signup`,
+  login: `/auth/signin`,
+  getTodos: `/todos`,
+  getTodoById: `/todos`,
+  createTodo: `/todos`,
+  updateTodo: `/todos`,
+  deleteTodo: `/todos`,
+};
