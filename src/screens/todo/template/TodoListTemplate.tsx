828139a75d149ae9bbd2c554f12806145609063e
HAKJOON0223 <2hakjoon@gmail.com>
2022-08-17 15:45:51 +0900

    STYLE - space for scrollbar

--- src/screens/todo/template/TodoListTemplate.tsx
+++ src/screens/todo/template/TodoListTemplate.tsx
@@ -17,15 +17,16 @@ const ListContainer = styled.ul`
   height: 100%;
   overflow-y: scroll;
   overflow-x: hidden;
+  padding-right: 5px;
   &::-webkit-scrollbar {
-    width: 10px; /*스크롤바의 너비*/
+    width: 5px; /*스크롤바의 너비*/
   }
-
+  
   &::-webkit-scrollbar-thumb {
     border-radius: 10px;
     background-color: gray; /*스크롤바의 색상*/
   }
-
+  
   &::-webkit-scrollbar-track {
     background-color: none; /*스크롤바 트랙 색상*/
   }
